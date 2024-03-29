8f968270e4f83bb23da750ee690bd36496da9563
Ben Fortuna <fortuna@nodelogic.au>
2024-03-29 14:15:07 +1100

    Added footer links

--- components/FooterLinks.vue
+++ components/FooterLinks.vue
@@ -1,3 +1,22 @@
 <template>
+        <div class="flex mx-auto items-center justify-center flex-wrap p-4 max-w-7xl">
+
+    <span class="flex flex-row mt-6 md:mt-0 mr-6">
+        <NuxtLink class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3" to="/advertise">
+            Advertise
+        </NuxtLink>
+    </span>
+    <span class="flex flex-row mt-6 md:mt-0 mr-6">
+        <NuxtLink class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3" to="/subscribe">
+            Subscribe
+        </NuxtLink>
+    </span>
+    <span class="flex flex-row mt-6 md:mt-0 mr-6">
+        <NuxtLink class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3" to="/contact">
+            Contact Us
+        </NuxtLink>
+    </span>
+        </div>
+
     <p class="text-gray-300 text-lg" style="text-align: center;">Copyright &copy; 2024 Waranga News, Inc.</p>
 </template>
