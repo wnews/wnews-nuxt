278ec254deeeb9d92fbf0921b828a63f8aed3c03
Ben Fortuna <fortuna@nodelogic.au>
2024-04-13 14:07:18 +1000

    Added footer banners

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,4 +2,5 @@
   <main>
     <ContentDoc class="p-5 space-y-4"/>
   </main>
+  <NuxtImg src="/images/advertise-banner.png"/>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,4 +3,5 @@
     <ContentDoc class="p-5 space-y-4" path="/past-issues/2024/2024-04-11"/>
   </main>
   <RecentPosts/>
+  <NuxtImg src="/images/advertise-banner.png"/>
 </template>
