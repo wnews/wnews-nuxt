9b301cbf4e2717a3cc68afd7588a10ac8b011997
Ben Fortuna <fortuna@nodelogic.au>
2024-04-13 19:24:13 +1000

    Migrate older posts

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,6 @@
   <main>
     <ContentDoc class="p-5 space-y-4" path="/past-issues/2024/2024-04-11"/>
   </main>
-  <RecentPosts :basePath="`${$route.fullPath}articles/2024`"/>
+  <RecentPosts :basePath="`${$route.fullPath + 'articles/2024'}`"/>
   <NuxtImg src="/images/advertise-banner.png"/>
 </template>
