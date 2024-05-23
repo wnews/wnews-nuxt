1247c255dad152ad95df92c59ce1e2d1e2ad1a0d
Ben Fortuna <fortuna@nodelogic.au>
2024-05-24 01:13:02 +1000

    Updates from latest issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pl-8">
-    <ContentDoc class="space-y-4 pb-8" path="/past-issues/2024/2024-05-09"/>
+    <ContentDoc class="space-y-4 pb-8" path="/past-issues/2024/2024-05-23"/>
     <RecentPosts :basePath="`${$route.fullPath + 'articles/2024'}`"/>
     <NuxtImg src="/images/advertise-banner.png"/>
   </div>
