675b14b6841a47625a80a78da44d6f300b6f85d8
Ben Fortuna <fortuna@nodelogic.au>
2024-05-25 01:20:28 +1000

    Redesigned footer

--- components/FooterLinks.vue
+++ components/FooterLinks.vue
@@ -1,24 +1,24 @@
 <template>
-    <footer>
+    <footer class="bg-black">
         <div class="flex mx-auto items-center justify-center flex-wrap p-4 max-w-7xl">
 
 <span class="flex flex-row mt-6 md:mt-0 mr-6">
-    <NuxtLink class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3" to="/advertise">
+    <NuxtLink class="inline-block text-neutral-500 hover:bg-gray-200 py-1 px-3" to="/advertise">
         Advertise
     </NuxtLink>
 </span>
 <span class="flex flex-row mt-6 md:mt-0 mr-6">
-    <NuxtLink class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3" to="/subscribe">
+    <NuxtLink class="inline-block text-neutral-500 hover:bg-gray-200 py-1 px-3" to="/subscribe">
         Subscribe
     </NuxtLink>
 </span>
 <span class="flex flex-row mt-6 md:mt-0 mr-6">
-    <NuxtLink class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3" to="/donations">
+    <NuxtLink class="inline-block text-neutral-500 hover:bg-gray-200 py-1 px-3" to="/donations">
         Donate
     </NuxtLink>
 </span>
 <span class="flex flex-row mt-6 md:mt-0 mr-6">
-    <NuxtLink class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3" to="/contact">
+    <NuxtLink class="inline-block text-neutral-500 hover:bg-gray-200 py-1 px-3" to="/contact">
         Contact Us
     </NuxtLink>
 </span>
