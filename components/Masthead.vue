b9c09fc254e59c92fcfacecc6ed58d74b9f7ebbf
Ben Fortuna <fortuna@nodelogic.au>
2024-05-27 09:21:16 +1000

    Adjustments for smallers screens

--- components/Masthead.vue
+++ components/Masthead.vue
@@ -8,7 +8,7 @@
         <div>
             <NuxtImg sizes="100vw md:1024px" class="max-w-xl" src="/images/wnews-masthead2.png" alt="Waranga News masthead image"/>
         </div>
-        <div class="flex items-center gap-3">
+        <div class="flex flex-wrap items-center gap-x-3 px-4">
             <NuxtLink href="/subscribe">Subscribe</NuxtLink>
             <NuxtLink href="/donations" class="bg-black text-white rounded-full px-4 py-2">Become a supporter</NuxtLink>
         </div>
--- components/NavigationTop.vue
+++ components/NavigationTop.vue
@@ -1,8 +1,8 @@
 <template>
     <nav id="navigation"
-    class="bg-black sticky top-0 z-40 font-body shadow-card hover:shadow-cardhover transition-all duration-500 md:text-xl text-neutral-400">
+    class="bg-black sticky top-0 z-40 font-body shadow-card hover:shadow-cardhover transition-all duration-500 md:text-lg text-neutral-400">
 <!-- Container to give Navbar a fixed width -->
-    <div class="flex gap-4 mx-auto items-center justify-between flex-wrap px-4 max-w-7xl">
+    <div class="flex md:gap-2 mx-auto items-center justify-between flex-wrap px-4 max-w-7xl">
          <NuxtLink class="inline-block hover:bg-gray-200 p-2" to="/">
             Home
         </NuxtLink>
@@ -34,12 +34,6 @@
          <NuxtLink class="inline-block p-2 hover:bg-gray-200" to="/about">
             About Us
         </NuxtLink>
-
-      <div class="flex gap-3">
-        <NuxtLink href="https://www.facebook.com/WarangaNews" title="Facebook" target="_blank"><Icon name="logos:facebook" class="size-7"/></NuxtLink>
-        <NuxtLink href="https://www.linkedin.com/company/waranga-news-inc." title="LinkedIn" target="_blank"><Icon name="logos:linkedin-icon" class="size-7 bg-white rounded"/></NuxtLink>
-        <NuxtLink href="https://www.instagram.com/waranganews/" title="Instagram" target="_blank"><Icon name="logos:instagram-icon" class="size-7 bg-white rounded-full"/></NuxtLink>
-      </div>
     </div>
     <!-- <Weather></Weather> -->
 </nav>
